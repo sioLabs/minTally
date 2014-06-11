51b885a484dbcd0144ed12c1f9696857d1bc912b
Ashutosh Singh <dh_ashu@outlook.com>
2014-06-11 21:41:56 +0530

    Added the code for the report. the code is working but file is now showing

--- src/innuinfocomm/CustMiniStmtController.java
+++ src/innuinfocomm/CustMiniStmtController.java
@@ -1,28 +1,119 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package innuinfocomm;
 
+import java.sql.Connection;
 import java.net.URL;
+import java.sql.DriverManager;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.ResourceBundle;
-import javafx.fxml.Initializable;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import net.sf.jasperreports.engine.JasperFillManager;
+import pojos.Customer;
+import utils.EntityManagerHelper;
 
-/**
- * FXML Controller class
- *
- * @author PIR
- */
-public class CustMiniStmtController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+
+
+
+public class CustMiniStmtController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ListView<Customer> custListView;
+
+    @FXML
+    private TextField searchCustTextField;
+
+
+    @FXML
+    void initialize() {
+        assert custListView != null : "fx:id=\"custListView\" was not injected: check your FXML file 'CustMiniStmt.fxml'.";
+        assert searchCustTextField != null : "fx:id=\"searchCustTextField\" was not injected: check your FXML file 'CustMiniStmt.fxml'.";
+        
+        start();
+
+    }
     
-}
+    public void start(){
+        
+        searchCustTextField.setOnKeyPressed(new EventHandler<KeyEvent>() {
+
+              @Override
+              public void handle(KeyEvent t) {
+                     if (KeyCode.TAB.equals(t.getCode())) {
+                           showCustomers("");
+                           return;
+                              }
+                 showCustomers(searchCustTextField.getText());
+                 if(KeyCode.ENTER.equals(t.getCode())){
+                     showCustomers(searchCustTextField.getText());
+                 }
+              }
+              });
+        
+        
+        custListView.setOnMouseClicked(new EventHandler<MouseEvent>(){
+
+            @Override
+            public void handle(MouseEvent t) {
+                Customer c = custListView.getSelectionModel().getSelectedItem();
+                
+                //now send this to jsp viewer
+                
+                try{
+                    Class.forName("com.mysql.jdbc.Driver");
+                    Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/innuinfocomm?user=root&password=");
+                    System.out.println("Filling report");
+                    HashMap p = new HashMap();
+                    p.put("custid", c.getId());
+                    JasperFillManager.fillReportToFile("src/reports/custMiniStmt.jasper", p, con);
+                            
+                    
+                    
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
+                
+            }
+            
+        });
+                
+    }
+                
+      public void showCustomers(String text){
+          
+          custListView.getItems().clear();
+          
+           EntityManager em = EntityManagerHelper.getInstance().getEm();
+           Query q  = null;
+           if(text == null || text.length() == 0)
+               q = em.createNamedQuery("Customer.findAll");
+           else{
+            text = text.trim();
+            text = "%"+text+"%";
+            q= em.createNamedQuery("Customer.findByCustomersNameLike");
+                q.setParameter("companyName", text);
+           }
+           
+            ArrayList<Customer> list = new ArrayList(q.getResultList());
+          //  System.out.println(list.size()+ " items found");
+            custListView.getItems().addAll(list);
+         
+          
+      }
+                
+                
+   }
